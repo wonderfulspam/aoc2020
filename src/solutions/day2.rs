f211ca6ae4a971322e5d57656dab788c7a1b935b
Emil Thorenfeldt <emt@magenta-aps.dk>
2020-12-02 18:20:49 +0100

    Day2, slower but cleaner

--- src/solutions/day2.rs
+++ src/solutions/day2.rs
@@ -1,52 +1,38 @@
-use std::str::FromStr;
+use parse_display::FromStr;
 
 const INPUT: &str = include_str!("../inputs/day2");
 
+#[derive(FromStr)]
+#[display("{min}-{max} {character}: {password}")]
 struct PasswordLine {
     min: usize,
     max: usize,
-    character: String,
+    character: char,
     password: String,
 }
 
-impl FromStr for PasswordLine {
-    type Err = Box<dyn std::error::Error>;
+fn filter1(entry: &PasswordLine) -> bool {
+    let matches = entry.password.matches(entry.character).count();
+    matches >= entry.min && matches <= entry.max
+}
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (first, password) = s.split_once(": ").unwrap();
-        let (min_max, character) = first.split_once(" ").unwrap();
-        let (min, max) = min_max.split_once("-").unwrap();
-        Ok(PasswordLine {
-            min: min.parse().unwrap(),
-            max: max.parse().unwrap(),
-            character: character.to_string(),
-            password: password.to_string()
-        })
-    }
+fn filter2(entry: &PasswordLine) -> bool {
+    let first_match = entry.password.chars().nth(entry.min - 1).unwrap() == entry.character;
+    let second_match = entry.password.chars().nth(entry.max - 1).unwrap() == entry.character;
+    first_match ^ second_match
 }
 
 pub fn run() {
-    let entries: Vec<PasswordLine> = INPUT
+    let count = INPUT
         .lines()
         .map(|l| l.parse::<PasswordLine>().unwrap())
-        .collect();
-    let mut counter = 0;
-    for entry in entries.iter() {
-        let count = entry.password.matches(&entry.character).count();
-        if count >= entry.min && count <= entry.max {
-            counter += 1;
-        }
-    }
-    println!("{}", counter);
+        .filter(|entry| filter1(entry)).count();
+    println!("{}", count);
 
-    counter = 0;
-    for entry in entries.iter() {
-        let first_match = entry.password.chars().nth(entry.min - 1).unwrap().to_string() == entry.character;
-        let second_match = entry.password.chars().nth(entry.max - 1).unwrap().to_string() == entry.character;
-        if first_match ^ second_match {
-            counter += 1;
-        }
-    }
+    let count = INPUT
+        .lines()
+        .map(|l| l.parse::<PasswordLine>().unwrap())
+        .filter(|entry| filter2(entry)).count();
+    println!("{}", count);
 
-    println!("{}", counter);
 }
