61ee36483f121ab0363e7bc90e3f76d4d6640fe1
Emil Thorenfeldt <emt@magenta-aps.dk>
2020-12-09 00:27:29 +0100

    Prevent parsing twice for almost 2x speedup

--- src/solutions/day5.rs
+++ src/solutions/day5.rs
@@ -1,10 +1,9 @@
 const INPUT: &str = include_str!("../inputs/day5");
 
 pub fn run() -> (String, String) {
-    let max = seat_iter().max().unwrap();
-
-    let mut seats = seat_iter().collect::<Vec<u32>>();
+    let mut seats = parse_seats(INPUT);
     seats.sort_unstable();
+    let max = seats.iter().max().unwrap();
     let seat = seats
         .windows(2)
         // Find first pair where difference in seat ID isn't 1
@@ -14,15 +13,17 @@ pub fn run() -> (String, String) {
     (max.to_string(), seat.to_string())
 }
 
-fn seat_iter() -> impl Iterator<Item = u32> {
-    INPUT.lines().map(|l| {
-        // Convert strings to u32 by bit shifting
-        // No need to multiply by 8; adding the last three digits
-        // will automatically take care of it
-        l.chars().fold(0, |acc, c| match c {
-            'F' | 'L' => (acc << 1),
-            'B' | 'R' => (acc << 1) | 1,
-            _ => unreachable!(),
+fn parse_seats(s: &str) -> Vec<u32> {
+    s.lines()
+        .map(|l| {
+            // Convert strings to u32 by bit shifting
+            // No need to multiply by 8; adding the last three digits
+            // will automatically take care of it
+            l.chars().fold(0, |acc, c| match c {
+                'F' | 'L' => (acc << 1),
+                'B' | 'R' => (acc << 1) | 1,
+                _ => unreachable!(),
+            })
         })
-    })
+        .collect()
 }
