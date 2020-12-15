02c4e9652b042f7c15b2a0d4ab17535776c961ab
Emil Thorenfeldt <emt@magenta-aps.dk>
2020-12-16 00:05:05 +0100

    Split numbers into vec and hash for 3x speedup

--- src/solutions/day15.rs
+++ src/solutions/day15.rs
@@ -1,10 +1,13 @@
 use ahash::AHashMap as HashMap;
 
 const INPUT: [u32; 7] = [0, 1, 5, 10, 3, 12, 19];
+const PART1: u32 = 2020;
+const PART2: u32 = 30_000_000;
+const BOUNDARY: u32 = PART2 / 20;
 
 pub fn run() -> (String, String) {
-    let part1 = solve_map(&INPUT, 2020);
-    let part2 = solve_map(&INPUT, 30_000_000);
+    let part1 = solve_map(&INPUT, PART1);
+    let part2 = solve_30m(&INPUT);
     (part1.to_string(), part2.to_string())
 }
 
@@ -16,6 +19,27 @@ fn solve_map(input: &[u32], turns: u32) -> u32 {
         .collect();
     (input.len() as u32..turns).fold(*input.last().unwrap(), |last, index| {
         index - history.insert(last, index).unwrap_or(index)
+    })
+}
+
+// Split bookkeeping into vec and hash, using a vec for the common (lower) numbers
+fn solve_30m(input: &[u32]) -> u32 {
+    let mut vec_history: [u32; BOUNDARY as usize] = [0; BOUNDARY as usize];
+    input
+        .iter()
+        .enumerate()
+        .for_each(|(i, &v)| vec_history[v as usize] = i as u32 + 1);
+    let mut hash_history: HashMap<u32, u32> = HashMap::with_capacity(1024 * 256);
+
+    (input.len() as u32..PART2).fold(*input.last().unwrap(), |last, index| {
+        if last < BOUNDARY {
+            let num = &mut vec_history[last as usize];
+            let current = if *num == 0 { 0 } else { index - *num };
+            *num = index;
+            current
+        } else {
+            index - hash_history.insert(last, index).unwrap_or(index)
+        }
     })
 }
 
